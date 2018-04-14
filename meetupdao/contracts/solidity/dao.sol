d2ecaf591449791b083c614d9eaeda91b86ac47e
pvram <35174958+pvram@users.noreply.github.com>
2018-04-14 16:41:43 +0530

    Update dao.sol

--- meetupdao/contracts/solidity/dao.sol
+++ meetupdao/contracts/solidity/dao.sol
@@ -115,6 +115,13 @@ contract BlockchainDevs is owned, tokenRecipient {
         return proposals.length;
     }   
 
+    function proposaltiming(uint _proposalTime, address _totalMembers) public
+    {
+        beneficiary = _totalMembers;
+        proposalEnd = now + _proposalTime;
+    }
+
+
     /**
      * Become member
      *
